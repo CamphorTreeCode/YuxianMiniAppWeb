d75fe9d3538ca054af8de5a7811d5a542fd2ce9f
LiChengLong <6025242704@qq.com>
2018-02-23 17:48:29 +0800

    修改位置的js，微信的头像

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,14 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    nickName:'',
+    avatarUrl:'',
+    gender:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that =this
+    wx.getUserInfo({
+      success: function (res) {
+        var userInfo = res.userInfo
+        var nickName = userInfo.nickName
+        var avatarUrl = userInfo.avatarUrl
+        var gender = userInfo.gender //性别 0：未知、1：男、2：女
+        avatarUrl = avatarUrl.replace('https', 'http');
+      that.setData({
+        nickName: nickName,
+        avatarUrl: avatarUrl,
+        gender:gender
+      })
+      }
+      
+    })
   },
 
   /**
